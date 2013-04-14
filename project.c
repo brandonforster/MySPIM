93b84cefd4d65c97ad41b3ef71d3770cd2ad77ec
Michael Andacht <mandacht3@yahoo.com>
2013-04-13 22:17:12 -0400

    All functions filled(not exactly working)
    Filled most of the functions not sure if working.

--- project.c
+++ project.c
@@ -19,7 +19,20 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+	//Check the proper part for the memory index
+	unsigned MemoryIndex = PC >> 2;
+    
+	//Check if it in proper word-aligned format (multiple of 4)
+    if (PC % 4 == 0) {     
+           
+           *instruction = Mem[MemoryIndex];
+           
+           return 0;
+           
+           }
+	//If not Halt
+    else 
+    return 1; 
 }
 
 
@@ -36,7 +49,29 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+	//Switch for different Op codes to decode 
+	//TODO get proper OP codes and proper values
+	switch(op) {
+	case 000: 
+		break;
+	case 001:
+		break;
+	case 010:
+		break;
+	case 011: 
+		break;
+	case 100:  
+		break;
+	case 101:  
+		break;
+	case 110: 
+		break;
+	case 111:  
+		break;
+	default:
+		return 1;
+	}
+	return 0;
 }
 
 /* Read Register */
@@ -51,7 +86,8 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+	//Not sure this is right
+	*extended_value = offset; 
 }
 
 /* ALU operations */
@@ -65,7 +101,16 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
+	//Possiblly right seems to simple.
+	if (MemRead == 1) {
+		*memdata = Mem[ALUresult];
+    }
 
+    if (MemWrite == 1) {
+		Mem[ALUresult] = data2;
+    }
+                           
+    return 0; 
 }
 
 
@@ -73,7 +118,26 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+	 // If Mem to Register
+     if (MemtoReg == 1 && RegDst == 0) {
+		Reg[r2] = memdata;
+     }
 
+	 //If Mem to Register but r3
+
+	 else if(MemtoReg == 1 && RegDst == 1){
+		 Reg[r3] = memdata;
+	 }
+     
+	 // If Result to Register
+     else if (MemtoReg == 0 && RegDst == 0) {
+		Reg[r2] = ALUresult;
+     }
+                  
+	 // If Result to Register but next value
+     else if (MemtoReg == 0 && RegDst == 1){
+		Reg[r3] = ALUresult;
+     }
 }
 
 /* PC update */
